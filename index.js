aa6e6b7a6d6558f0d4890a247f01ed7a9f1db40b
ulises avila camargo <ulisesavila04@gmail.com>
2023-02-08 18:49:53 -0600

    add basic server

---
+++ index.js
@@ -0,0 +1,16 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit: "2mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "2mb", extended: true }));
+
+app.use(function (err, req, res, next) {
+  console.log("Last error", err);
+});
+
+app.listen(5000, () => console.log("Server is up"));
